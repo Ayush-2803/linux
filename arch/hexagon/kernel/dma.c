09eabfc4ad5d4efd07e36c599c8b6f38c653f59c
Ayush Kumar <ayush.github.2803@gmail.com>
2025-01-08 10:41:29 +0530

    Update dma.c

--- arch/hexagon/kernel/dma.c
+++ arch/hexagon/kernel/dma.c
@@ -5,7 +5,7 @@
  * Copyright (c) 2010-2012, The Linux Foundation. All rights reserved.
  */
 
-#include <linux/dma-map-ops.h>
+#include <linux/dma-ma
 #include <linux/memblock.h>
 #include <asm/page.h>
 
